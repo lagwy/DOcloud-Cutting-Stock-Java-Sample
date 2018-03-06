d72dd82d247d1dca4b0143d823ecb58fd251a112
Luis Ángel Martínez <luis.0795@hotmail.com>
2018-03-06 02:07:27 -0600

    JSON parse

--- src/main/java/com/ibm/optim/oaas/sample/cuttingStock/Server.java
+++ src/main/java/com/ibm/optim/oaas/sample/cuttingStock/Server.java
@@ -7,12 +7,19 @@ import java.net.InetSocketAddress;
 import java.util.*;
 import java.io.InputStream;
 import java.lang.Throwable;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.optim.oaas.sample.cuttingStock.Server.SolutionHandler.Row;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Server {
+
+    // Mapper for Java --> JSON serialization
+	protected static ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws Exception {
         int port = 8000;
@@ -67,6 +74,12 @@ public class Server {
             while (is.read() != -1);
             is.close();
 
+            Requirement actualObj = mapper.readValue(body, Requirement.class);
+
+            System.out.println( actualObj.possible.toString() );
+            System.out.println( actualObj.desired.toString() );
+
+
             t.sendResponseHeaders(200, body.length());
             t.getResponseHeaders().set("Content-Type", "application/json");
 
@@ -74,6 +87,33 @@ public class Server {
             OutputStream os = t.getResponseBody();;
             os.write( body.getBytes() );
             os.close();
+        }
+
+        public static class Requirement {
+            @JsonProperty("DesiredLengths")
+            public List<Row> desired;
+
+            @JsonProperty("PossibleLengths")
+            public List<Integer> possible;
+
+        }
+
+        public static class Row{
+            @JsonProperty("length")
+            public int length;
+
+            @JsonProperty("quantity")
+            public int quantity;
+
+            public Row(){
+                
+            }
+
+            public Row(int length, int quantity){
+                this.length = length;
+                this.quantity = quantity;
+            }
+
         }
     }
 
